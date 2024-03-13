6cdd3bb525a363cd179a3f380af282163abb85b4
unknown <zozozld@naver.com>
2024-03-13 16:54:25 +0900

    24-03-13 수정본

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
+import { Route, Routes, Link, useNavigate, useParams } from "react-router-dom";
+
+function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,4 @@
+const Notfound = () => {
+  return <div>알 수 없는 페이지.</div>;
+};
+export default Notfound;
