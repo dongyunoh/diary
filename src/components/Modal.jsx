c5098fcbcd77f695d84b2f825abec10b155af536
unknown <zozozld@naver.com>
2024-04-30 23:03:59 +0900

    Modal 가슴운동 특징 정리

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,17 @@
-import { getPressImage } from "../util/get-press-image";
-import { useState } from "react";
-
 import "./Modal.css";
+import chest1 from "./../assets/chest1.png";
 
 const Modal = () => {
-  // const [chestBtn, setChestBtn] = useState("가슴운동");
-
   return (
-    <div className="Modal">
-      <h2>벤치프레스</h2>
-      {/* <div className={`img_section img_section_${actionId}`}>
-        <img src={getPressImage(0)} />
-      </div> */}
+    <div className="chest_List_wrapper">
+      <img src={chest1} alt="Chest lnc. logo" />
+      <p>
+        스쿼트, 데드리프트와 함께 웨이트 트레이닝을 대표하는 3대 운동 중
+        하나이다. <hr></hr> 그 중에서 벤치 프레스는 웨이트 트레이닝을 해본 적이
+        없는 사람들도 알고 있는 경우가 많을 정도로 가장 널리 알려진 운동으로서,
+        대흉근과 삼두근을 위주로 가슴 부근의 상체 근육들을 종합적으로 강화하기
+        위한 최고의 운동이다.
+      </p>
     </div>
   );
 };
