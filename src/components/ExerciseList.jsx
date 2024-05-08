9f13f31900040069f4873ae7e5d24da29ea87671
unknown <zozozld@naver.com>
2024-05-08 21:36:27 +0900

    24-05-08

--- src/components/ExerciseList.jsx
+++ src/components/ExerciseList.jsx
@@ -1,37 +1,63 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Route, Routes, Link, NavLink } from "react-router-dom";
+import Chest from "../pages/Chest";
+// import ChestList from "./ChestList";
 
 import Button from "./Button";
 
 import "./ExerciseList.css";
 
-const ExerciseList = () => {
-  const [choice, setChoice] = useState("chest");
-  const nav = useNavigate();
+const activeStyle = {
+  color: "red",
+  textDecoration: "underline",
+};
 
-  const fruits = ["chest", "back", "button", "shouder"];
+const deactiveStyle = {
+  color: "black",
+  textDecoration: "none",
+};
 
-  const options = fruits.map((fruit, idx) => {
-    return (
-      <option key={idx} value={fruit}>
-        {fruit}
-      </option>
-    );
-  });
-  console.log(choice);
-  const handleFruit = (event) => {
-    setChoice(event.target.value);
-  };
+function ExerciseList() {
+  // const nav = useNavigate();
 
   return (
     <div className="exerciseList">
-      <select value={choice} onChange={handleFruit}>
-        {options}
-      </select>
-      {choice}
-      <Button onClick={() => nav("/chest")} text="chest" />
+      <p>
+        <NavLink
+          to="/chest"
+          style={({ isActive }) => {
+            return isActive ? activeStyle : deactiveStyle;
+          }}
+        >
+          Chest
+        </NavLink>
+      </p>
+      <p>
+        <NavLink
+          to="/login"
+          className={({ isActive }) => {
+            return isActive ? "orange" : "";
+          }}
+        >
+          Login
+        </NavLink>
+      </p>
+      <hr />
+
+      <Routes>
+        <Route path="/chest" element={<Chest />}></Route>
+      </Routes>
+      {/* <div className="exercies_button">
+        <div className="chest">
+          <Button onClick={() => nav("/chest")} text="chest" />
+        </div>
+        <div className="back">
+          <Button onClick={() => nav("/back")} text="back" />
+        </div>
+      </div>
+      <div className="exercies_screen"></div> */}
     </div>
   );
-};
+}
 
 export default ExerciseList;
